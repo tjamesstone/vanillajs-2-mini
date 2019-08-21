2333c4999a88752c63de47b081dc437e6725a602
Tate Stone <tatejamesstone@gmail.com>
2019-08-21 13:53:21 -0600

    work

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+document.querySelector('form').addEventListener('submit', addTodo)
+
 function addTodo (event){
+    event.preventDefault()
     const item = document.createElement('li')
     const input = document.querySelector('#item')
     item.innerText = input.value 
+    item.addEventListener('click', completeTodo)
+
+    const button = document.createElement('button')
+    button.innerText = 'X'
+    button.addEventListener('click', removeTodo)
+    item.append(button)
+
+    
+
     const list = document.querySelector("ul")
     list.appendChild(item)
 }
 
-window.addEventListener
+function removeTodo (event){
+    event.target.parentNode.remove()
+}
+
+function completeTodo (event){
+    if (event.target.getAttribute('aria-checked') !== "true") {
+        event.target.setAttribute('aria-checked', "true")
+    }else {
+        event.target.setAttribute('aria-checked', "false")
+    }
+
+}
